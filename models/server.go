d0e9fff32fb2545e8c81cc48ccfa64de7f1d7f58
wangcheng2016 <wangcheng347@163.com>
2019-03-12 13:21:37 +0800

    redis init

--- models/server.go
+++ models/server.go
@@ -3,6 +3,7 @@ package models
 import (
 	"github.com/astaxie/beego/logs"
 	"github.com/garyburd/redigo/redis"
+	"strconv"
 	"time"
 )
 
@@ -10,24 +11,24 @@ var (
 	seckillconf *SecKillConf
 )
 
-func initBlackRedis() (err error) {
-	seckillconf.BlackRedisPool = &redis.Pool{
-		MaxIdle:     seckillconf.RedisBlackConf.RedisMaxIdle,
-		MaxActive:   seckillconf.RedisBlackConf.RedisMaxActive,
-		IdleTimeout: time.Duration(seckillconf.RedisBlackConf.RedisIdleTimeout) * time.Second,
+func initRedis(conf RedisConf) (redisPool  *redis.Pool,err error) {
+	pool := &redis.Pool{
+		MaxIdle: conf.RedisMaxIdle,
+		MaxActive: conf.RedisMaxActive,
+		IdleTimeout: time.Duration(conf.RedisIdleTimeout) * time.Second,
 		Dial: func() (redis.Conn, error) {
-			return redis.Dial("tcp", seckillconf.RedisBlackConf.RedisAddr)
+			return redis.Dial("tcp",conf.RedisAddr)
 		},
 	}
+	conn := pool.Get()
+	defer  conn.Close()
 
-	conn := seckillconf.BlackRedisPool.Get()
-	defer conn.Close()
-
-	_, err = conn.Do("ping")
+	_,err = conn.Do("ping")
 	if err != nil {
-		logs.Error("ping readis failed,err :%v", err)
+		logs.Error("ping redis failed,err :%v", err)
 		return
 	}
+	redisPool = pool
 	return
 }
 
@@ -35,11 +36,53 @@ func loadBlackList() (err error) {
 	seckillconf.ipBlackMap = make(map[string]bool, 10000)
 	seckillconf.idBlackMap = make(map[int]bool, 10000)
 
+	pool,err := initRedis(seckillconf.RedisBlackConf)
+	if err != nil {
+		logs.Error("init black redis failed,err: %v",err)
+		return
+	}
+	seckillconf.BlackRedisPool = pool
+
+	conn := seckillconf.BlackRedisPool.Get()
+	defer  conn.Close()
+
+	relply,err := conn.Do("hgetall","idblacklist")
+	idlist,err := redis.Strings(relply,err)
+	if err != nil {
+		logs.Warn("hget all failed,err:%v",err)
+		return
+	}
+
+	for _,v := range idlist {
+		id,err := strconv.Atoi(v)
+		if err != nil {
+			logs.Warn("invalid user id: %v",id)
+			continue
+		}
+		seckillconf.idBlackMap[id] = true
+	}
+
+	relply ,err = conn.Do("hgetall","ipblacklist")
+	iplist,err := redis.Strings(relply,err)
+	if err != nil {
+		logs.Warn("hget all failed,err:%v",err)
+		return
+	}
+
+	for _,v := range iplist {
+		seckillconf.ipBlackMap[v] = true
+	}
 	return
 }
 
 func InitServer(secKillConfig *SecKillConf) (err error) {
 	seckillconf = secKillConfig
 
+	err = loadBlackList()
+	if err != nil {
+		logs.Error("load black list err: %v",err)
+		return
+	}
+	logs.Debug("init service success,config: %v",seckillconf)
 	return
 }
